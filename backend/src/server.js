5b4a529452a0abe4797e9eeede1c97e73b3e1af8
Kenji Yamamoto <contato@kenjiyamamoto.com>
2019-08-06 23:51:58 -0300

    Seman 8 - Dia 02

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0-rcvgd.mongodb.net/omnistack8?retryWrites=true&w=majority',{useNewUrlParser: true})
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
