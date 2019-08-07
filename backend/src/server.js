6cbb705ebbfe60fd6faf095ce85561e110ce0f78
Kenji Yamamoto <contato@kenjiyamamoto.com>
2019-08-06 23:57:11 -0300

    Add Cors no projeto

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0-rcvgd.mongodb.net/omnistack8?retryWrites=true&w=majority',{useNewUrlParser: true})
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
